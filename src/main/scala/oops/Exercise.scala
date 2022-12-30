a6ab18927ad532f8fee4c6dcc4334c181471422c
Gokul-sv <gokulksp@gmail.com>
2022-12-30 08:44:09 +0530

    Scala OOPs Exercise

---
+++ src/main/scala/oops/Exercise.scala
@@ -0,0 +1,22 @@
+package oops
+
+import scala.language.postfixOps
+
+object Exercise extends App {
+  val author = new Writer("James", "Gosling", 1955)
+  val novel = new Novel("Java", 1995, author)
+
+  println(novel.authorAge())
+  println(novel.isWrittenBy(author))
+  println(novel.copy(1996))
+}
+
+class Writer(val firstname: String, val surname: String, val year: Int) {
+  def fullName(): String = s"$firstname $surname"
+}
+
+class Novel(val name: String, val yearOfRel: Int, val author: Writer) {
+  def authorAge(): Int = yearOfRel - author.year
+  def isWrittenBy(author: Writer): Boolean = this.author == author
+  def copy(newYearOfRel: Int) : Novel = new Novel(name, newYearOfRel, author)
+}
